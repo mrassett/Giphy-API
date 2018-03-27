965744e4308724127f6d49ed3795aa0267450556
mrassett <magsrassett@gmail.com>
2018-03-26 19:38:11 -0500

    Almost all elements working

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-"use strict";
 
-
-
-//AJAX call to GIPHY using key provided
 //iterate AJAX call around button click to pull 10 GIFs from GIPHY
 //write function to click start video
 //write function to click stop vide
 let existingButtons = ["Black Mirror", "Narcos", "Stranger Things", "13 Reasons Why", "Ozark", "Shameless"];
-
 
 //create array, loop over each string and create standing buttons from the string objects
 
@@ -15,6 +10,9 @@ let existingButtons = ["Black Mirror", "Narcos", "Stranger Things", "13 Reasons 
         $("#button-well").empty(); 
         for (let i = 0; i < existingButtons.length; i++) {
                 let generateButton = $("<button>");
+                generateButton.addClass("tv");
+                generateButton.addClass("btn btn-info")
+                generateButton.attr("data-name", existingButtons[i]);
                 generateButton.html(existingButtons[i]);
                 console.log(generateButton);
                 $("#button-well").append(generateButton);
@@ -30,7 +28,45 @@ let existingButtons = ["Black Mirror", "Narcos", "Stranger Things", "13 Reasons 
         console.log(tvShowEntry);
         $("#enter-show").empty();
 });
-console.log(buttonCreate);
-buttonCreate();
+        buttonCreate();
+
+
+
+
+
+// Event listener for all button elements
+$("button").on("click", function () {
+
+    let netflix = $(this).attr("data-name");
+    // let netflix = $(this).attr("data-tv");
 
 //AJAX call to GIPHY using key provided
+apiKey = "TognscCDCvIkZorbQ3YSYr1F937ccMw7";
+
+let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + netflix + "&limit=10";
+
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+
+// After data comes back from the request
+    .then(function(response){
+    let results = response.data;    
+    for (let i = 0; i < results.length; i++){
+    let newGifDiv = $("<div>");
+    newGifDiv.addClass("newGifDiv");
+
+console.log(queryURL);
+
+    let gifImage = $("<img>");
+    gifImage.attr("src", results[i].images.fixed_height.url); 
+    newGifDiv.append(gifImage);
+    $("#images").prepend(gifImage);
+
+console.log(response);
+console.log(results);
+}
+})
+});
